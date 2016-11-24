e9bb8593b01a66f4cfa8e72f07f17b9cf6d89f1a
Philipp Fehr <calvin.philipp@gmail.com>
2016-11-24 15:09:22 +0100

    Presentation restruction finallyzing

---
+++ src/view/Renderer.java
@@ -0,0 +1,12 @@
+package view;
+
+/**
+ * Created by Philipp on 24/11/2016.
+ */
+public class Renderer {
+
+
+
+
+
+}
---
+++ src/view/input/MouseInput.java
@@ -0,0 +1,13 @@
+package view.input;
+
+import org.lwjgl.glfw.GLFWCursorPosCallback;
+
+public class MouseInput extends GLFWCursorPosCallback {
+
+	@Override
+	public void invoke(long window, double xpos, double ypos) {
+//		System.out.println("X: " + xpos + "\nY: " + ypos);
+		
+	}
+	
+}
